f3f63ef9416a9e676bba43e7f1a9abe9b0ee0bbc
GordonLiWei <gordonliwei@hotmail.com>
2015-01-27 16:22:19 +0800

    Gordon加入了一些視覺化UI元件

--- uMainForm.pas
+++ uMainForm.pas
@@ -4,10 +4,18 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
+  FMX.TabControl, FMX.StdCtrls, FMX.Controls.Presentation;
 
 type
   TForm3 = class(TForm)
+    ToolBar1: TToolBar;
+    Button1: TButton;
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    TabItem2: TTabItem;
+    TabItem3: TTabItem;
+    Edit1: TEdit;
   private
     { Private declarations }
   public
